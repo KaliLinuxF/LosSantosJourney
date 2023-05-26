420e70d1e755f3f61e965f217b52ecfe2c4792e0
davinchi59 <mirnuy1296@mail.ru>
2023-05-26 20:40:58 +0700

    Sound system complete

--- ui/src/App.tsx
+++ ui/src/App.tsx
@@ -3,7 +3,57 @@ import './index.sass'
 import rpc from 'rage-rpc'
 import Auth from "./interfaces/Auth";
 import Notifications from "./interfaces/Notifications";
+import { SOUNDS_LIST } from './utils/audio/list';
 import {useAppDispatch} from "./hooks/redux";
+import {Howl} from "howler";
+
+// @ts-ignore
+window.soundSystem = {
+	list: {},
+	banList: [],
+}
+
+// @ts-ignore
+const playSound = ({ file, volume = 0.1, loop = false }) => {
+	// @ts-ignore
+	const { list } = window.soundSystem;
+	const dir = file.split('/')[0];
+	const isListHasFile = SOUNDS_LIST.hasOwnProperty(file);
+	const isFileLooped = loop && list.hasOwnProperty(file);
+	// @ts-ignore
+	const isFileBanned = ~window.soundSystem.banList.indexOf(dir);
+	if (!isListHasFile || isFileLooped || isFileBanned) return;
+	let sound = new Howl({
+		// @ts-ignore
+		src: [SOUNDS_LIST[file]],
+		autoplay: true,
+		loop,
+		volume,
+		onend: () => {
+			if(!loop) {
+				// @ts-ignore
+				this.unload()
+			}
+		},
+	});
+	if (!loop) return sound.off();
+	list[file] = sound;
+};
+// @ts-ignore
+window.soundSystem.playSound = playSound;
+
+const stopSound = (file: string) => {
+	// @ts-ignore
+	const { list } = window.soundSystem;
+	if (!list.hasOwnProperty(file)) return;
+	list[file].unload();
+	delete list[file];
+};
+// @ts-ignore
+window.soundSystem.stopSound = stopSound;
+
+// @ts-ignore
+Object.keys(SOUNDS_LIST).forEach((file) => window.soundSystem.playSound({ file, volume: 0 }));
 
 const App: React.FC = () => {
 	const dispatch = useAppDispatch()
---
+++ ui/src/utils/audio/list.ts
@@ -0,0 +1,6 @@
+// @ts-ignore
+import AuthBg from './files/auth/bg.mp3'
+
+export const SOUNDS_LIST = {
+	'auth/bg': AuthBg,
+}
